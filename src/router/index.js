5a015b5ae756ccf5dc76e5513789d06872844930
Pilar Fernandez Nieto <rla10309@educastur.es>
2024-05-05 21:09:06 +0200

    Primer commit

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Inicio from '../views/ClientesView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "inicio",
+      component: Inicio,
+      props: {
+        titulo: "Listado de Clientes",
+      },
+    },
+    {
+      path: "/agregar-cliente",
+      name: "agregar-cliente",
+      component: () => import("../views/NuevoClienteView.vue"),
+      props: {
+        titulo: "Nuevo Cliente",
+      },
+    },
+  ],
+});
+
+export default router
